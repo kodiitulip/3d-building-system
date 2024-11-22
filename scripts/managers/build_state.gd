56c589b1396e8be77bbb2738dc3e690ffb2e31f6
Kodie <kodii.tulip@gmail.com>
2024-11-22 13:42:22 -0300

    moving files and adjusting build sys

--- scripts/managers/build_state.gd
+++ scripts/managers/build_state.gd
@@ -1,19 +1,23 @@
 class_name BuildStateManager
-extends GridMap
+extends Node3D
 
-const RAY_LENGHT: float = 3000.0
-const MOVEMENT_AMOUNT: float = 5.0
+@export var ray_length: float = 3000.0
+@export var movement_time: float = 5.0
+@export_group("Centered", "centered_")
+@export var centered_cell_x: bool = false
+@export var centered_cell_y: bool = false
+@export var centered_cell_z: bool = false
 
 var current_spawnable: Node3D
 var current_offset: Vector3 = Vector3(0.0, 0.0, 0.0)
 var container: Node3D
 var new_pos: Vector3
 
+var plane: Plane = Plane.PLANE_XZ
 
 func _ready() -> void:
-	cell_center_y = false
-	cell_size = Vector3.ONE
 	container = get_tree().get_first_node_in_group(&"world_container")
+	plane.d = global_position.y
 
 
 func _process(delta: float) -> void:
@@ -25,7 +29,22 @@ func _process(delta: float) -> void:
 		new_pos = map_to_local(grid_pos)
 		new_pos.y = maxf(new_pos.y, 0)
 		current_spawnable.global_position = current_spawnable.global_position\
-			.lerp(new_pos, delta * MOVEMENT_AMOUNT)
+			.lerp(new_pos, delta * movement_time)
+
+
+func local_to_map(pos: Vector3) -> Vector3i:
+	return Vector3i(floori(pos.x),floori(pos.y),floori(pos.z))
+
+
+func map_to_local(grid_pos: Vector3i) -> Vector3:
+	var final: Vector3 = Vector3(grid_pos)
+	if centered_cell_x:
+		final += Vector3(0.5, 0.0, 0.0)
+	if centered_cell_y:
+		final += Vector3(0.0, 0.5, 0.0)
+	if centered_cell_z:
+		final += Vector3(0.0, 0.0, 0.5)
+	return final
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -70,5 +89,5 @@ func _get_mouse_world_position() -> Vector3:
 	var camera: Camera3D = get_viewport().get_camera_3d()
 	var screen_point: Vector2 = get_viewport().get_mouse_position()
 	var pos: Vector3 = Utils.cast_ray_to_plane(
-		Plane(Vector3.UP), screen_point, camera, RAY_LENGHT)
+		plane, screen_point, camera, ray_length)
 	return pos
